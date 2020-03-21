b8fe8e5806170bead233a9ab64665a984827e224
shreedhar16197 <harsh16041@iiitd.ac.in>
2020-03-21 07:29:58 -0400

    update

--- deepwalk/deepwalk.py
+++ deepwalk/deepwalk.py
@@ -1,5 +1,6 @@
 from graph import Graph
 from parameters import SoftmaxTree, EmbeddingMatrix
+from tqdm import tqdm
 import torch
 import random
 import torch.optim as optim
@@ -29,26 +30,26 @@ def skipgram(random_walk, graph, tree, embedding, optimT, optimE):
             
 def deepwalk(tree, embedding, graph, optimT, optimE):
     nodes_index = [i for i in range(graph.num_nodes)]
-    for gamma in range(1, args.walks_per_vertex+1):
+    for gamma in tqdm(range(1, args.walks_per_vertex+1)):
         random.shuffle(nodes_index)
-        for counter,vi in enumerate(nodes_index):
+        for counter,vi in tqdm(enumerate(nodes_index)):
             print("iter {} vertex {}".format(gamma, counter))
             vi_name = graph.idx_to_name_map[vi]
             random_walk = graph.random_walk(vi_name, args.walk_length)
             tree,embedding,optimT,optimE = skipgram(random_walk, graph, tree, embedding, optimT, optimE)
-            break
     return tree,embedding,optimT,optimE
 if __name__ == "__main__":
     g = Graph("../data/nodes.csv", "../data/edges.csv")
     degreelist = g.get_degrees()
     degreelist = sorted(degreelist, key = lambda x : x[1])
-    tree = SoftmaxTree(embed_size = args.embed_size).to(device)
+    tree = SoftmaxTree(embed_size = args.embed_size)
     tree.create_huffman(degreelist)
+    tree = tree.to(device)
     embeddings = EmbeddingMatrix(max_nodes = args.max_nodes, embed_size = args.embed_size).to(device)
     optimE = optim.Adam(embeddings.parameters(), lr = args.lr)
     optimT = optim.Adam(tree.parameters(), lr = args.lr)
     tree,embeddings,optimT,optimE = deepwalk(tree, embeddings, g, optimT, optimE)
-    embeddings = list(embeddings.parameters())[0].data.numpy()
+    embeddings = list(embeddings.parameters())[0].data.cpu().numpy()
     
     f = open("embeddings.pkl", "wb")
     pickle.dump(embeddings, f)
