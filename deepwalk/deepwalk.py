fix softmax harsh

deepwalk/deepwalk.py
@@ -24,9 +24,8 @@ def write_walk(random_walk, tree, f, data_counter):
         for j in range(max(0, idx-args.window_length), min(len(random_walk), idx + args.window_length + 1)):
             input_node_idx = random_walk[j]
             context_idx = random_walk[idx]
-            index_path_tree = tree.get_path(input_node_idx)
-            x = np.array([context_idx] + index_path_tree)
-            f[str(data_counter)] = x
+            index_path_tree, binary_multipliers = tree.get_path(input_node_idx)
+            f[str(data_counter)] = np.array([context_idx] + index_path_tree + binary_multipliers)
             data_counter += 1 
     return data_counter
 def deepwalk(tree,graph):
@@ -53,18 +52,22 @@ if __name__ == "__main__":
     optimE = optim.Adam(embeddings.parameters(), lr = args.lr)
     optimT = optim.Adam(tree.parameters(), lr = args.lr)
     deepwalk(tree,g)
+    #print(tree.codes)
     dataset = WalkPairData(args.hdf5file)
     train_loader = DataLoader(dataset, batch_size = 1, num_workers = 1, shuffle = True)
     totalsize = len(train_loader)
-    for e in range(1):
+    for e in range(3):
         for idx,batch in (enumerate(train_loader)):
             batch = Variable(batch.type(Tensor), requires_grad = False)
             context_idx = batch[:,0]
-            tree_path_idx = batch[:,1:].squeeze(0)
+            length = batch.shape[1] - 1
+            length //= 2
+            tree_path_idx = batch[:,1:1+length].squeeze(0)
+            binary_multipliers = batch[:,1+length:].squeeze(0)
             optimE.zero_grad()
             optimT.zero_grad()
             context_vector = embeddings(context_idx)
-            prob = tree(context_vector, tree_path_idx)
+            prob = tree(context_vector, tree_path_idx,binary_multipliers)
             loss = nll(prob)
             print(idx, totalsize,loss,prob)
             loss.backward()

deepwalk/parameters.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 from heapq import heapify, heappush, heappop 
 import copy
 import sys
+import numpy as np
+import args
 class Node:
     def __init__(self):
         self.left = None
@@ -66,29 +68,31 @@ class SoftmaxTree(nn.Module):
         self.generate_codes(self.root, [])
         self.inorder_traversal(self.root)
         self.matrix = nn.Embedding(self.size, self.embed_size)
-    
+        self.matrix.weight.data.copy_(torch.from_numpy(np.random.uniform(low = args.low_weight/self.embed_size, high = args.low_weight/self.embed_size, size = (self.size, self.embed_size))))
     def get_path(self, input_node_idx):
         code = self.codes[input_node_idx]
         root = self.root
         ret = []
+        mul = []
         for i in code:
             ret.append(root.index)
             if(i == 0):
                 root = root.left
+                mul.append(1)
             else:
                 root = root.right
-        return ret
-    def forward(self, context_embedding, input_path_idxs):
+                mul.append(-1)
+        return ret,mul
+    def forward(self, context_embedding, input_path_idxs, binary_multiplier):
         input_vectors = self.matrix(input_path_idxs)
         context_embedding = torch.transpose(context_embedding, 1, 0)
         #print(input_vectors, context_embedding)
         probs = torch.mm(input_vectors, context_embedding)
-        #print(probs)
-        probs /= torch.norm(context_embedding)
         probs = probs.squeeze(1)
-        probs /= torch.norm(input_vectors, dim = 1)    
-        #print(probs)
-        probs = (1 + probs)/2
+        binary_multiplier = binary_multiplier.squeeze(0)
+        probs = probs * binary_multiplier
+        #print(probs.squeeze(0))
+        probs = torch.sigmoid(probs)
         prob = torch.prod(probs)
         #print(prob)
         return prob
@@ -98,5 +102,7 @@ class EmbeddingMatrix(nn.Module):
         self.max_nodes = max_nodes
         self.embed_size = embed_size
         self.matrix = nn.Embedding(self.max_nodes, self.embed_size)
+        self.matrix.weight.data.copy_(torch.from_numpy(np.random.uniform(low = args.low_weight/self.embed_size, high = args.high_weight/self.embed_size, size = (self.max_nodes, self.embed_size))))
+    
     def forward(self, input):
         return self.matrix(input)