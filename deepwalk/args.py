9173690888bc307eae8f38e124d919bf48214f5e
Harsh Pathak <harsh16041@iiitd.ac.in>
2020-05-09 14:13:52 +0530

    Update args.py

--- deepwalk/args.py
+++ deepwalk/args.py
@@ -8,8 +8,5 @@ low_weight = -0.5
 high_weight = 0.5 
 tree_constructor = "complete" #choose from complete or huffman
 use_old_copy = False #if true, doesnt create new walk_pairs.hdf5
-#NOTE - using huffman or complete tree coding creates entirely different trees and hence different walk_pairs.hdf5 files
-#this means that walk_pairs of huffman coded tree cant be used for complete tree
-#Further, huffman support batch size 1 where as complete supports any batch size
 batch_size = 1 if tree_constructor == "complete" else 1
 subset_size = -1
